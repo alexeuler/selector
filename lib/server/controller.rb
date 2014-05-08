569ad27f1b7aed92c0e7747bfc19801bf37fe711
alexey <karasev.alexey@gmail.com>
2014-05-08 17:14:28 +0400

    changed protocol

--- lib/server/controller.rb
+++ lib/server/controller.rb
@@ -3,18 +3,29 @@ require_relative "../selector"
 
 class Controller
   def call(env)
-    RubyProf.start
+    #RubyProf.start
     puts "#{Time.now}: Received request: #{env['REQUEST_URI']}"
     params = params(env)
-    user_id = params["user_id"].to_i
-    Selector::select(user_id) if user_id>0
-    puts "#{Time.now}: Done"
-    result = RubyProf.stop
-    printer = RubyProf::GraphHtmlPrinter.new(result)
-    File.open(App.root+"/profiling/profile.html", "w") {|file| printer.print(file)}
+    task = task(env)
+    case task
+      when "train"
+        user_id=params["user_id"].to_i
+        return [200, {}, ["Invalid user_id: #{user_id}"]] if user_id <=0
+        train(user_id)
+    end
 
+    puts "#{Time.now}: Done"
+    # result = RubyProf.stop
+    # printer = RubyProf::GraphHtmlPrinter.new(result)
+    # File.open(App.root+"/profiling/profile.html", "w") {|file| printer.print(file)}
     [200, {}, []]
   end
+
+  def train(user_id)
+    Selector::select(user_id) if user_id>0
+  end
+
+  private
 
   def params(env)
     data = env["QUERY_STRING"].split("&")
@@ -24,5 +35,9 @@ class Controller
       hash[key] = value
     end
     hash
+  end
+
+  def task(env)
+    env["REQUEST_PATH"][1..-1]
   end
 end
