eece8170eb245aa0326bc384acc6cd42f7f639cd
alexey <karasev.alexey@gmail.com>
2014-05-08 17:33:59 +0400

    added lock for user

--- lib/selector.rb
+++ lib/selector.rb
@@ -2,7 +2,14 @@ require_relative "selector/trainer"
 
 module Selector
 
+  MINIMUM_TRAINING_TIME_INTERVAL = 10 #in seconds
+
   def self.select(user_id)
+    #If this user's model is already being trained - reject
+    time_lock = App.redis.hget("lock:train", user_id).to_i
+    return if time_lock != nil and
+        Time.now.to_i - time_lock < MINIMUM_TRAINING_TIME_INTERVAL
+    App.redis.hset("lock:train", user_id, Time.now.to_i)
     trainer = Trainer.new(redis: App.redis, features_collection: App.features)
     trainer.train(user_id)
   end
