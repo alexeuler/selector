5571d6ac18b781a284917f2548c78184148ac358
alexey <karasev.alexey@gmail.com>
2014-05-14 15:13:45 +0400

    removed redis auth

--- lib/config/boot.rb
+++ lib/config/boot.rb
@@ -15,5 +15,4 @@ puts "Done"
 fetcher.async.start
 App.redis = Redis.new(:host => App.config.db.front.host , :port => App.config.db.front.port,
                       :db => App.config.db.front.name)
-App.redis.auth(App.config.db.front.password)
 
